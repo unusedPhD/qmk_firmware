05232d565d75ab8bff83984343d341ee4770792e
Stefan Amyotte <stefan.amyotte@gmail.com>
2016-09-20 06:48:10 -0400

    moved lower to left thumb

--- keyboards/planck/keymaps/unusedphd/keymap.c
+++ keyboards/planck/keymaps/unusedphd/keymap.c
@@ -12,8 +12,9 @@ extern keymap_config_t keymap_config;
 #define XXXXXXX KC_NO
 
 // Custom macros
-#define CTL_ESC  CTL_T(KC_ESC)  // Tap for Esc, hold for Ctrl
-#define SFT_ENT  SFT_T(KC_ENT)  // Tap for Enter, hold for Shift
+#define CTL_ESC  CTL_T(KC_ESC)      // Tap for Esc, hold for Ctrl
+#define SFT_ENT  SFT_T(KC_ENT)      // Tap for Enter, hold for Shift
+#define SPFN     LT(_RAISE, KC_SPC) // press for space, hold for function layer (aka spacefn)
 
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
 #define _QWERTY 0
@@ -45,14 +46,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
   {CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
   {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_ENT},
-  {KC_LEAD, KC_LCTL, KC_LGUI, KC_LALT, SWITCH,  LOWER,   RAISE,   KC_SPC,  MOVE,    KC_MINS, KC_UNDS, KC_PENT}
+  {KC_LEAD, KC_LCTL, KC_LGUI, KC_LALT, LOWER,   SWITCH,  RAISE,   SPFN,    MOVE,    KC_MINS, KC_UNDS, KC_PENT}
 },
 
 [_NUMPAD] = {
   {_______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_7,    KC_8,    KC_9,    KC_PSLS, KC_BSPC},
   {_______, XXXXXXX, XXXXXXX, _______, XXXXXXX, XXXXXXX, XXXXXXX, KC_4,    KC_5,    KC_6,    KC_PAST, KC_DEL},
   {XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_1,    KC_2,    KC_3,    KC_PMNS, KC_PEQL},
-  {XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, XXXXXXX, XXXXXXX, XXXXXXX, KC_0,    KC_DOT,  KC_PPLS, KC_PENT}
+  {XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, XXXXXXX, XXXXXXX, KC_0,    KC_DOT,  KC_PPLS, KC_PENT}
 },
 
 [_VNUMPAD] = {
