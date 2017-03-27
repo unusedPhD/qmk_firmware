7f8a5e11ab2ff5f28bbc69b846333e29b3cfec07
Stefan Amyotte <stefan.amyotte@gmail.com>
2017-03-26 21:13:29 -0400

    spacing tweak

--- keyboards/planck/keymaps/unusedphd/keymap.c
+++ keyboards/planck/keymaps/unusedphd/keymap.c
@@ -13,8 +13,9 @@ extern keymap_config_t keymap_config;
 
 // Custom macros
 #define CTL_ESC  CTL_T(KC_ESC)       // Tap for Esc, hold for Ctrl
-#define SFT_ENT  SFT_T(KC_ENT)       // Tap for Enter, hold for Shift
-#define SFT_BSP  SFT_T(KC_BSPC)      // Tap for Enter, hold for Shift
+#define SFT_SP   SFT_T(KC_SPC)       // Tap for space, hold for Shift
+#define SFT_BSP  SFT_T(KC_BSPC)      // Tap for backspace, hold for Shift
+#define SFT_ENT  SFT_T(KC_ENT)      // Tap for Enter, hold for Shift
 #define SP_DW    LT(_LOWER, KC_SPC)  // Tap for space, hold for lower layer
 #define SP_UP    LT(_RAISE, KC_SPC)  // Tap for space, hold for raise layer
 #define LFT_MVE  LT(_MOVE, KC_LEFT)  // Tap for space, hold for raise layer
@@ -38,17 +39,17 @@ enum planck_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = {
-  {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,       KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC},
-  {CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,       KC_J,    KC_K,    KC_L, KC_SCLN, KC_ENT},
-  {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,       KC_M, KC_COMM,  KC_DOT, KC_SLSH, SFT_ENT},
-  {NPAD,    KC_LCTL, KC_LGUI, KC_LALT, SP_DW,   SFT_BSP, SFT_ENT,   SP_UP, LFT_MVE, KC_DOWN,   KC_UP, KC_RGHT}
+  {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
+  {CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT},
+  {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_ENT},
+  {NPAD,    KC_LCTL, KC_LGUI, KC_LALT, SP_DW,   SFT_BSP, SFT_SP,  SP_UP,   LFT_MVE, KC_DOWN, KC_UP,   KC_RGHT}
 },
 
 [_NUMPAD] = {
-  {_______, _______, _______, _______, _______, _______,    KC_7,    KC_8,    KC_9, _______, KC_PSLS, KC_BSPC},
-  {_______, _______, _______, _______, _______, _______,    KC_4,    KC_5,    KC_6, _______, KC_PAST, KC_DEL},
-  {_______, _______, _______, _______, _______, _______,    KC_1,    KC_2,    KC_3, _______, KC_PMNS, KC_PEQL},
-  {_______, _______, _______, _______, _______, _______,    KC_0,    KC_0,  KC_DOT, _______, KC_PPLS, KC_PENT}
+  {_______, _______, _______, _______, _______, _______, KC_7,    KC_8,    KC_9,    _______, KC_PSLS, KC_BSPC},
+  {_______, _______, _______, _______, _______, _______, KC_4,    KC_5,    KC_6,    _______, KC_PAST, KC_DEL},
+  {_______, _______, _______, _______, _______, _______, KC_1,    KC_2,    KC_3,    _______, KC_PMNS, KC_PEQL},
+  {_______, _______, _______, _______, _______, _______, KC_0,    KC_0,    KC_DOT,  _______, KC_PPLS, KC_PENT}
 },
 
 [_LOWER] = {
